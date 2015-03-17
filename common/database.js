b6334282e917c07bdc5731c29d648b5d3f214d0d
mooveprince <mooveprince@gmail.com>
2015-03-16 21:07:20 -0500

    dbcode

---
+++ common/database.js
@@ -0,0 +1,31 @@
+var pg = require('pg');
+
+var conString = process.env.DATABASE_URL;
+var client = new pg.Client(conString);
+client.connect();
+
+exports.getAccessToken = function (application, callback) {
+    var sql = "SELECT accesstoken FROM oauth where appname = $1 ";
+    client.query(sql, [application], function (err, result) {
+        if (err) {
+            console.log ("Error in executing the query");
+            callback(true);
+        } else {
+            console.log("Result.." + result.rows[0].accesstoken);
+            callback (false, result.rows[0].accesstoken);
+        }  
+    });
+}
+
+exports.setAccessToken = function (application, accesstoken, callback) {
+    var sql = "UPDATE oauth SET accesstoken = $1 where appname = $2"
+    client.query (sql, [accesstoken, application], function (err, result) {
+        if (err) {
+            console.log ("Error in executing the query");
+            callback();
+        } else {
+            console.log("Inserted Successfully");
+            callback ( );
+        }
+    });
+}
--- routes/phroute.js
+++ routes/phroute.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var query = require('../common/database');
+
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.json ({value:'Product Hunt Values'});
+    query.getAccessToken ('PRODUCTHUNT',function(err, accesstoken) {
+        if (err) {
+            res.json({error: 'Some error occurred while getting access token from DB'});
+        } else {
+            console.log ('Access token is ' + accesstoken);
+            res.json ({value:'Product Hunt Values'});
+        }
+        //res.json ({value:'Product Hunt Values'});
+    });
 });
 
 module.exports = router;
