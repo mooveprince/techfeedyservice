eb601b7c7e5a2968fbd0ff0b4a01066722c03e60
mooveprince <mooveprince@gmail.com>
2015-03-21 22:54:35 -0500

    techmeme

--- routes/techmemeroute.js
+++ routes/techmemeroute.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var feedExtractor = require('../common/rssfeedextractor');
+var feedUrl = 'http://techmeme.com/feed.xml';
+
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.json ({value:'Tech meme Values'});
+    feedExtractor.getTopTrend (feedUrl, function (itemResult) {
+        console.log ("Sending the response for TECHMEME");
+        if (req.query.limit >= 0 ) {
+            res.json (itemResult.slice(0, req.query.limit));
+        } else {
+            res.json (itemResult);
+        }
+    });
+    
 });
 
 module.exports = router;
-
