2fae6c4328e6a9c17b0be0ec3a2f2504f5fcb877
mooveprince2@gmail.com <mooveprince2@gmail.com>
2015-03-24 15:05:12 -0500

    AddedHeader

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
+
 app.use('/producthunt', phRoute);
 app.use('/hackernews',hackernewsRoute);
 app.use('/techcrunch',techcrunchRoute);
